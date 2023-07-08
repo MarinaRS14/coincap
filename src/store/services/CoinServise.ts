f78ffad85363c95874fa072ab106f780c634bab3
Марина Ровбо <marinar@MarinaRS.local>
2023-07-08 19:32:04 +0300

    10 commit

--- src/store/services/CoinServise.ts
+++ src/store/services/CoinServise.ts
@@ -2,9 +2,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IData } from '../../types/IData';
 import { ICoin } from '../../types/ICoin';
 
+const BASE = process.env.REACT_APP_BASE_URL;
+
 export const coinAPI = createApi({
   reducerPath: 'coinAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.coincap.io/v2' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE }),
   endpoints: (build) => ({
     fetchAllCoins: build.query<IData, number>({
       query: (limit: number) => ({
